09cd1101e138d55f41e6b7bd466e5aa609570cc1
Malcolm Cephas <137897091+malcolm-cephas@users.noreply.github.com>
2026-04-06 10:51:54 +0530

    fixes v3.3

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,8 @@ import type { NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 export async function middleware(request: NextRequest) {
-  const client = auth0;
-  if (client) {
-    return await client.middleware(request);
+  if (auth0) {
+    return await (auth0 as NonNullable<typeof auth0>).middleware(request);
   }
 }
 
